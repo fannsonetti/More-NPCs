1b22a4409b149960a34ffd357b608c2f390055f0
fannsonetti <morerabasimnetis@gmail.com>
2025-11-10 15:28:21 +0000

    Refactor OfficerMarcus NPC configuration and appearance

--- NPCs/OfficerMarcus.cs
+++ NPCs/OfficerMarcus.cs
@@ -1,14 +1,19 @@
-﻿using MelonLoader;
+using MelonLoader;
 using S1API.Entities;
 using S1API.Entities.Schedule;
 using S1API.Map;
+using S1API.Map.ParkingLots;
 using S1API.Money;
 using S1API.Economy;
-using S1API.Entities.NPCs.Suburbia;
+using S1API.Entities.NPCs.Northtown;
 using S1API.GameTime;
+using S1API.Growing;
+using S1API.Map.Buildings;
 using S1API.Products;
 using S1API.Properties;
+using S1API.Vehicles;
 using UnityEngine;
+using System.Linq;
 
 namespace CustomNPCTest.NPCs
 {
@@ -18,64 +23,106 @@ namespace CustomNPCTest.NPCs
     /// </summary>
     public sealed class OfficerMarcus : NPC
     {
-        protected override bool IsPhysical => true;
+        public override bool IsPhysical => true;
 
         protected override void ConfigurePrefab(NPCPrefabBuilder builder)
         {
-            Vector3 pos1 = new Vector3(27.4168f, 1.065f, -14.4305f);
-            Vector3 pos2 = new Vector3(-22.5378f, 1.065f, -43.1838f);
-            Vector3 pos3 = new Vector3(-150.581f, -2.935f, 118.1681f);
-            Vector3 pos4 = new Vector3(-139.3423f, -4.335f, 18.8808f);
-            Vector3 pos5 = new Vector3(-121.0444f, -2.935f, 78.6722f);
-            Vector3 pos6 = new Vector3(-22.7021f, 1.065f, 46.8433f);
+            var policeStation = Building.Get<PoliceStation>();
+            MelonLogger.Msg("Configuring prefab for NPC 1");
+            Vector3 posA = new Vector3(27.4168f, 1.065f, -14.4305f);
+            Vector3 posB = new Vector3(-22.5378f, 1.065f, -43.1838f);
+            Vector3 posC = new Vector3(-150.581f, -2.935f, 118.1681f);
+            Vector3 posD = new Vector3(-139.3423f, -4.335f, 18.8808f);
+            Vector3 posE = new Vector3(-121.0444f, -2.935f, 78.6722f);
+            Vector3 posF = new Vector3(-22.7021f, 1.065f, 46.8433f);
             Vector3 spawnPos = new Vector3(16.0717f, 1.065f, 38.0883f);
-            builder.WithSpawnPosition(spawnPos)
+            builder.WithIdentity("officer_marcus", "Officer", "Marcus")
+                .WithAppearanceDefaults(av =>
+                {
+                    var skinColor = new Color(0.713f, 0.592f, 0.486f);
+                    av.Gender = 0.0f;
+                    av.Height = 1.0f;
+                    av.Weight = 0.4f;
+                    av.SkinColor = skinColor;
+                    av.LeftEyeLidColor = av.SkinColor;
+                    av.RightEyeLidColor = av.SkinColor;
+                    av.EyeBallTint = Color.white;
+                    av.PupilDilation = 1.0f;
+                    av.EyebrowScale = 1.1f;
+                    av.EyebrowThickness = 1.0f;
+                    av.EyebrowRestingHeight = -0.432f;
+                    av.EyebrowRestingAngle = -2.451f;
+                    av.LeftEye = (0.219f, 0.5f);
+                    av.RightEye = (0.219f, 0.5f);
+                    av.HairColor = new Color(0.31f, 0.2f, 0.12f);
+                    av.HairPath = "Avatar/Hair/Buzzcut/Buzzcut";
+                    av.WithFaceLayer("Avatar/Layers/Face/Face_NeutralPout", Color.black);
+                    av.WithFaceLayer("Avatar/Layers/Face/OldPersonWrinkles", new Color32(0, 0, 0, 0));
+                    av.WithFaceLayer("Avatar/Layers/Face/FacialHair", new Color(0.31f, 0.2f, 0.12f));
+                    av.WithBodyLayer("Avatar/Layers/Top/RolledButtonUp", new Color(0.178f, 0.217f, 0.406f));
+                    av.WithBodyLayer("Avatar/Layers/Bottom/Jeans", new Color(0.235f, 0.235f, 0.235f));
+                    av.WithAccessoryLayer("Avatar/Accessories/Feet/CombatBoots/CombatBoots", new Color(0.151f, 0.151f, 0.151f));
+                    av.WithAccessoryLayer("Avatar/Accessories/Waist/PoliceBelt/PoliceBelt", Color.white);
+                    av.WithAccessoryLayer("Avatar/Accessories/Chest/BulletproofVest/BulletproofVest_Police", Color.white);
+                })
+                .WithSpawnPosition(spawnPos)
                 .EnsureCustomer()
                 .WithCustomerDefaults(cd =>
                 {
-                    cd.WithSpending(minWeekly: 800f, maxWeekly: 1200f)
-                        .WithOrdersPerWeek(1, 5)
+                    cd.WithSpending(minWeekly: 400f, maxWeekly: 1000f)
+                        .WithOrdersPerWeek(1, 4)
                         .WithPreferredOrderDay(Day.Sunday)
-                        .WithOrderTime(2330)
-                        .WithStandards(CustomerStandard.High)
+                        .WithOrderTime(900)
+                        .WithStandards(CustomerStandard.VeryLow)
                         .AllowDirectApproach(true)
-                        .GuaranteeFirstSample(false)
+                        .GuaranteeFirstSample(true)
                         .WithMutualRelationRequirement(minAt50: 2.5f, maxAt100: 4.0f)
-                        .WithCallPoliceChance(0.90f)
-                        .WithDependence(baseAddiction: 0.0f, dependenceMultiplier: 1.1f)
+                        .WithCallPoliceChance(0.15f)
+                        .WithDependence(baseAddiction: 0.1f, dependenceMultiplier: 1.1f)
                         .WithAffinities(new[]
                         {
-                            (DrugType.Marijuana, 0.52f), (DrugType.Methamphetamine, -0.84f), (DrugType.Cocaine, -0.09f)
+                            (DrugType.Marijuana, 0.45f), (DrugType.Cocaine, -0.2f)
                         })
-                        .WithPreferredProperties(Property.Athletic, Property.AntiGravity, Property.Sneaky);
+                        // .WithPreferredPropertiesById("Munchies", "Energizing", "Cyclopean");
+                        .WithPreferredProperties(Property.Munchies, Property.Energizing, Property.Cyclopean);
                 })
                 .WithRelationshipDefaults(r =>
                 {
-                    r.WithDelta(1.0f)
+                    r.WithDelta(1.5f)
                         .SetUnlocked(false)
                         .SetUnlockType(NPCRelationship.UnlockType.DirectApproach)
-                        .WithConnectionsById("alison_knight", "carl_bundy", "jack_knight");
+                        // .WithConnectionsById("kyle_cooley", "ludwig_meyer", "austin_steiner")
+                        .WithConnections(Get<KyleCooley>(), Get<LudwigMeyer>(), Get<AustinSteiner>());
                 })
                 .WithSchedule(plan =>
                 {
-                    plan.EnsureDealSignal();
-                    plan.Add(new WalkToSpec { Destination = pos1, StartTime = 700, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos2, StartTime = 900, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos3, StartTime = 1100, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos4, StartTime = 1300, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos5, StartTime = 1500, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos6, StartTime = 1700, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos3, StartTime = 1900, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos5, StartTime = 2100, FaceDestinationDirection = true });
-                    plan.Add(new WalkToSpec { Destination = pos2, StartTime = 2300, FaceDestinationDirection = true });
-                    plan.Add(new StayInBuildingSpec { BuildingName = "Police Station", StartTime = 2400, DurationMinutes = 420 });
+                    plan.EnsureDealSignal()
+                        .WalkTo(posA, 700, faceDestinationDir: true)
+                        .WalkTo(posB, 900, faceDestinationDir: true)
+                        .WalkTo(posC, 1100, faceDestinationDir: true)
+                        .WalkTo(posD, 1300, faceDestinationDir: true)
+                        .WalkTo(posE, 1500, faceDestinationDir: true)
+                        .WalkTo(posF, 1700, faceDestinationDir: true)
+                        .WalkTo(posC, 1900, faceDestinationDir: true)
+                        .WalkTo(posE, 2100, faceDestinationDir: true)
+                        .WalkTo(posB, 2300, faceDestinationDir: true)
+                        .StayInBuilding(policeStation, 2400, 420);
+                })
+                .WithInventoryDefaults(inv =>
+                {
+                    // Startup items that will always be in inventory when spawned
+                    inv.WithStartupItems("m1911", "m1911mag")
+                        // Random cash between $50 and $500
+                        .WithRandomCash(min: 50, max: 500)
+                        // Preserve inventory across sleep cycles
+                        .WithClearInventoryEachNight(false);
                 });
         }
 
         public OfficerMarcus() : base(
             id: "officer_marcus",
-            firstName: "Officer Marcus",
-            lastName: "",
+            firstName: "Officer",
+            lastName: "Marcus",
             icon: null)
         {
         }
@@ -85,11 +132,10 @@ namespace CustomNPCTest.NPCs
             try
             {
                 base.OnCreated();
-                ApplyConsistentAppearance();
+                ApplyAppearance();
                 Appearance.Build();
-
-                Aggressiveness = 5f;
-                Region = Region.Suburbia;
+                Aggressiveness = 3f;
+                Region = Region.Northtown;
 
                 // Customer.RequestProduct();
 
@@ -104,9 +150,9 @@ namespace CustomNPCTest.NPCs
         }
 
         /// <summary>
-        /// Applies a consistent appearance. Tweak the values below to your liking.
+        /// Applies an appearance. Tweak the values below to your liking.
         /// </summary>
-        private void ApplyConsistentAppearance()
+        private void ApplyAppearance()
         {
             // Core biometrics
             Appearance
@@ -135,5 +181,3 @@ namespace CustomNPCTest.NPCs
         }
     }
 }
-
-
